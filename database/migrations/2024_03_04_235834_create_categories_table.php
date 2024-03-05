cbbc6ccf400f92da0d2c5db59fb991daf58405a9
Davixo23 <david.utic@gmail.com>
2024-03-04 20:31:02 -0400

    Creacion de migraciones

---
+++ database/migrations/2024_03_04_235834_create_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();// llave primaria y es autoincremental automatico controla laravel;
+            $table->string('title',60)->unique();// unicos
+            $table->string('alias',60)->unique();
+            $table->smallInteger('position',60);
+            $table->boolean('published');
+            $table->timestamps();
+            $table->string('created_by',60);
+            $table->string('updated_by',60)->nullable();// permite nulos
+            // se crea una tabla con essa definicion cuando se lea las migraciones
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
